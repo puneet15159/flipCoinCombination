229a2af880cd20acd941c525644e6b2da0104684
puneet <puneet15159@iiitd.ac.in>
2020-06-05 15:00:59 +0530

    doublet combination added

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -3,6 +3,8 @@
 declare -A dic
 counter=10
 
+#---------------------SINGLET COMBINATION------------------------------------------
+
 dic[H]=0
 dic[T]=0
 
@@ -19,6 +21,7 @@ do
 	fi
 	((counter--))
 done
+echo "-------------------SINGLET COMBINATION-------------------"
 echo "${!dic[@]}"
 echo "${dic[@]}"
 
@@ -29,3 +32,50 @@ calc ${dic[H]}/10*100
 
 echo "tails percentage"
 calc ${dic[T]}/10*100
+
+#---------------------------DOUBLET COMBINATION------------------------------------
+
+declare -A dic1
+dic1[HH]=0
+dic1[HT]=0
+dic1[TT]=0
+dic1[TH]=0
+counter=10
+
+while (( counter > 0 ))
+do
+        flip=$((RANDOM%4))
+        if [[ flip -eq 0 ]]
+        then
+                #echo "heads"
+                dic1[HH]=$((${dic1[HH]}+1))
+        elif [[ flip -eq 1 ]]
+	then
+                #echo "tails"
+                dic1[HT]=$((${dic1[HT]}+1))
+	elif [[ flip -eq 2 ]]
+	then
+		dic1[TT]=$((${dic1[TT]}+1))
+	elif [[ flip -eq 3 ]]
+	then
+		dic1[TH]=$((${dic1[TH]}+1))
+        fi
+        ((counter--))
+done
+echo "-------------------------DOUBLET COMBINATION--------------------------"
+echo "${!dic1[@]}"
+echo "${dic1[@]}"
+
+echo "HH percentage"
+calc ${dic1[HH]}/10*100
+
+echo "HT percentage"
+calc ${dic1[HT]}/10*100
+
+echo "TT percentage"
+calc ${dic1[TT]}/10*100
+
+echo "TH percentage"
+calc ${dic1[TH]}/10*100
+
+
